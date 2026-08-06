50e6c56f07fd7e82fdb6526aba023ae5c067c956
unknown <ikhsanloris@gmail.com>
2026-08-06 12:27:14 +0800

    fix bug import logic

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.egt51e8pep8"
+    "revision": "0.0oqe17e0l8g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
