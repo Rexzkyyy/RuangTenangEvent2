35aa46c368c26b7e356bd9f40af45d4edd03affa
unknown <ikhsanloris@gmail.com>
2026-08-08 13:31:55 +0800

    chore: update service worker revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8vig777lh7g"
+    "revision": "0.8h36prr4nug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
