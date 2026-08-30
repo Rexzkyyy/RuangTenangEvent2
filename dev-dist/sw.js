8a78c53a5400141e99d737dc8d520803141bb77f
unknown <ikhsanloris@gmail.com>
2026-08-30 09:58:23 +0800

    fix: use absolute URL for Open Graph image to fix WhatsApp preview

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c5gl9nfoqd"
+    "revision": "0.rk7rt6vb3r8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
